dcde67cdf3b2a871c6a43c6c8130bac07def6933
Brooks Bryant <brooks.bryant@icloud.com>
2024-06-22 22:07:21 -0500

    restructured files

--- Lexer.hs
+++ Lexer.hs
@@ -1,11 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
 
-module Main where
+module Lexer where
 
+import Control.Applicative
+import Data.Char
 import System.Environment
 import Data.Maybe
-import AParser
-import Control.Applicative
 import Text.Regex.Posix
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing
+    f (x:xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing
+
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+posInt :: Parser Int
+posInt = Parser f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x,y) = (f x, y)
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ (fmap $ first f) . p
+
+instance Applicative Parser where
+  pure a = Parser (\s -> Just (a, s))
+  (Parser fp) <*> xp = Parser $ \s ->
+    case fp s of
+      Nothing     -> Nothing
+      Just (f,s') -> runParser (f <$> xp) s'
+
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  (Parser p1) <|> (Parser p2) = Parser (\s -> p1 s <|> p2 s)
 
 data Token = IntLit Int
            | Ident String
@@ -19,10 +58,17 @@ data Token = IntLit Int
            | Semi
            deriving (Show)
 
-firstCharIsClosingBrace :: String -> Bool
-firstCharIsClosingBrace str = case str =~ "\\A\\}" :: Bool of
-    True -> True
-    False -> False
+spaces :: Parser String
+spaces = many (satisfy isSpace)
+
+-- assumes you'll only try to match the beginning of the string
+parseRegex :: String -> Parser String
+parseRegex regex = Parser f
+  where
+    f s
+      | s =~ regex = Just (match, drop (length match) s)
+      | otherwise = Nothing
+      where match = (s =~ regex) :: String
 
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
@@ -34,8 +80,10 @@ parseConstToken t r = (const t) <$> parseRegex r
 
 -- regex package was being weird for {} characters
 parseCharToken :: Token -> Char -> Parser Token
-parseCharToken t c = (const t) <$> satisfy (==c)
+parseCharToken t c = (const t) <$> char c
 
+
+-- hard to generalize because token values have different types
 parseIntLit :: Parser Token
 parseIntLit = (IntLit . read) <$> parseRegex "^[0-9]+\\b"
 
